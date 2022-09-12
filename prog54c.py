2281792ffcee6a5a0e0513a9bb6cbcd385ba31cd
Sophia Disch <112713343+SophiaDisch@users.noreply.github.com>
2022-09-12 05:09:26 +0000

    update

---
+++ prog54c.py
@@ -0,0 +1,8 @@
+radius = float(input("Radius: "))
+
+pi = 3.14159
+circumference = 2 * radius * pi
+area = (radius * radius) * pi
+
+print("\nCircumference: ", circumference)
+print("\nArea: ", area)
