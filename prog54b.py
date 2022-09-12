47cffcd91bf069f40eaf929357c4863c5b419775
Sophia Disch <112713343+SophiaDisch@users.noreply.github.com>
2022-09-12 03:47:33 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,10 @@
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number: "))
+num3 = int(input("Please enter the third number: "))
+num4 = int(input("Please enter the fourth number: "))
+
+sum = num1 + num2 + num3 + num4
+average = sum / 4
+
+print("\nThe sum is", sum)
+print("The average is", average)
