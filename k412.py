7392d6657c1c5efdeee5bb95bbc7036a309e1764
Sophia Disch <112713343+SophiaDisch@users.noreply.github.com>
2022-09-26 14:04:13 +0000

    update

--- k412.py
+++ k412.py
@@ -111,7 +111,22 @@ class ktools:
 def main():
     """ Karel code goes here! """
     kt = ktools()
-  
+    kt.mtl()
+    kt.put()
+    kt.ta()
+    kt.mtl()
+    kt.tl()
+    kt.m()
+    kt.put2()
+    kt.ta()
+    kt.mm(2)
+    kt.tl()
+    kt.mtl()
+    kt.putm(3)
+    kt.ta()
+    kt.mm(3)
+    kt.tl()
+    kt.mm(6)
     pass
 
 
